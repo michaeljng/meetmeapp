a2df78063795740da960e3ae92cbe50099226e84
Lucas Haber <lhaber9@gmail.com>
2015-11-05 00:09:31 -0500

    Added Facebook login via the ngOpenFb library for angular, seems to be working well. Showing users name after login

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,23 +1,43 @@
 angular.module('meetme.controllers', [])
 	
-	.controller('LoginController', function ($scope, $state, LoginService) {
+	.controller('LoginController', function ($scope, $state, ngFB) {
 
-		$scope.username = "";
-		$scope.password = "";
-		$scope.userId = null;
+		ngFB.init({appId: '1652380235023803'});
+
+		if (ngFB.getLoginStatus().$$state.value.status == 'connected') {
+			$state.go('logged-in');
+		}
 
 		$scope.doLogin = function() {
-			var userId = LoginService.login($scope.username,$scope.password);
-			if (userId) {
-				$scope.userId = userId;
-				$state.go('logged-in',{'userId':userId});
-			}
+			ngFB.login({scope: 'public_profile,email'}).then(
+                	function (response) {
+	                   	 if (response.status === 'connected') {
+	                        $state.go('logged-in');
+	                    } else {
+	                        alert('Facebook login failed');
+	                    }
+	                }
+	        );  
 		}
 
 	})
 
-	.controller('MainController', function ($scope, $stateParams) {
+	.controller('MainController', function ($scope, ngFB) {
 
-		
+		ngFB.init({appId: '1652380235023803'});
 
+		$scope.fbName = '';
+		$scope.nickName = '';
+
+		ngFB.api({
+			path: '/me',
+			param: {fields: 'name'}
+		}).then (
+			function(user) {
+				$scope.fbName = user.name;
+			},
+			function(error) {
+				console.log(JSON.stringify(error));
+				alert('Error connecting to Facebook. Did you log in?');
+			});
 	})
--- www/js/services.js
+++ www/js/services.js
@@ -1,12 +1,1 @@
 angular.module('meetme.services', [])
-
-	.factory('LoginService', function ($http) {
-
-		return {
-
-			login: function(username, password) {
-				return 18; // dummy login
-			}
-
-		}
-	})
