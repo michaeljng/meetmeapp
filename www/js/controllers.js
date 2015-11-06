30564161631bcfb8e02f730fb83c80187343a50a
Lucas Haber <lhaber9@gmail.com>
2015-11-06 00:46:52 -0500

    added Parse connection, added nickname field updating

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,9 @@
 angular.module('meetme.controllers', [])
 	
-	.controller('LoginController', function ($scope, $state, ngFB) {
+	.controller('LoginController', function ($scope, $state, ngFB, ParseService) {
 
 		ngFB.init({appId: '1652380235023803'});
-
+ 
 		if (ngFB.getLoginStatus().$$state.value.status == 'connected') {
 			$state.go('logged-in');
 		}
@@ -12,7 +12,22 @@ angular.module('meetme.controllers', [])
 			ngFB.login({scope: 'public_profile,email'}).then(
                 	function (response) {
 	                   	 if (response.status === 'connected') {
-	                        $state.go('logged-in');
+	                   	 	ngFB.api({
+								path: '/me',
+								param: {fields: 'id'}
+							}).then (
+								function(user) {
+									ParseService.get('Users', {"facebookId":user.id}, function(results) { 
+
+										if (results.length == 0) {
+											console.log(results.length);
+											ParseService.create('Users', {"facebookId":user.id})
+										}
+
+										$state.go('logged-in');
+									});
+							});
+	                   	 	
 	                    } else {
 	                        alert('Facebook login failed');
 	                    }
@@ -22,22 +37,38 @@ angular.module('meetme.controllers', [])
 
 	})
 
-	.controller('MainController', function ($scope, ngFB) {
+	.controller('MainController', function ($scope, $state, ngFB, ParseService) {
 
 		ngFB.init({appId: '1652380235023803'});
 
 		$scope.fbName = '';
 		$scope.nickName = '';
+		$scope.userId = '';
 
 		ngFB.api({
 			path: '/me',
-			param: {fields: 'name'}
+			param: {fields: 'name, id'}
 		}).then (
 			function(user) {
 				$scope.fbName = user.name;
+				
+				ParseService.get('Users', {"facebookId":user.id}, function(results) { 
+					$scope.nickName = results[0].nickName;
+					$scope.userId = results[0].objectId;
+				});
 			},
 			function(error) {
 				console.log(JSON.stringify(error));
-				alert('Error connecting to Facebook. Did you log in?');
-			});
+				$state.go('logged-out');
+		});
+
+		$scope.updateNickname = function() {
+			console.log("HRER");
+			ParseService.update('Users', $scope.userId,{"nickName":$scope.nickName});
+		}
+
+		$scope.logout = function() {
+			ngFB.logout();
+			$state.go('logged-out');
+		}
 	})
--- www/js/services.js
+++ www/js/services.js
@@ -1,1 +1,46 @@
 angular.module('meetme.services', [])
+	
+	.factory('ParseService', function ($http) {
+
+		var headers = {	"X-Parse-Application-Id": "EvhQWhNkOQrt9FOkJaEAe3tX5qJDfq7K8NMMnpd8",
+       					"X-Parse-REST-API-Key": "GPHw7mJbToX9Tyw7suXilsbkoUoSKN7wpXuTUqJK"}
+
+		return {
+
+			get: function(className, params, completion) {
+				$http({
+					method: 'GET',
+					url: 'https://api.parse.com/1/classes/' + className,
+					params: {"where" : params},
+					headers: headers
+				}).then( function successCallback(response) {
+			        // console.log(JSON.stringify(response));
+			    	completion(response.data.results);
+			    }, function errorCallback(response) {
+			    // called asynchronously if an error occurs
+			    // or server returns response with an error status.
+			    });
+			},
+
+			create: function(className, object) {
+				$http({
+					method: 'POST',
+					url: 'https://api.parse.com/1/classes/' + className,
+					data: object,
+					headers: headers
+				});
+			},
+
+			update: function(className, id, params) {
+				$http({
+					method: 'PUT',
+					url: 'https://api.parse.com/1/classes/' + className + '/' + id,
+					data: params,
+					headers: headers
+				});
+			}
+
+
+		}
+
+	});
