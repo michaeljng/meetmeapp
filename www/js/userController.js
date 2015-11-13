1eafa4b96518fde5f6213fc14a7b04501dceb7f0
Chris Clark <c.clark1928@gmail.com>
2015-11-13 15:50:02 -0500

    Restructure user-profile.html to use ionic tags and refactor userController.js to use ng-model correctly.

--- www/js/userController.js
+++ www/js/userController.js
@@ -21,39 +21,47 @@ angular.module('meetme.userController', [])
 
 .controller('UserController', function ($scope, $state, ParseService, FacebookService) {
 
-	$scope.fbId = '';
-	$scope.fbName = '';
-	$scope.nickName = '';
-	$scope.userId = '';
-	$scope.userDescription = '';
-	$scope.userLocation = '';
-	$scope.userAge = '';
+	$scope.user = {
+		fbId: '',
+		fbName: '',
+		nickName: '',
+		id: '',
+		description: '',
+		location: '',
+		age: ''
+	}
+	
 
 	FacebookService.getUserFields(['name', 'id'], function(user) {
-		$scope.fbId = user.id;
-		$scope.fbName = user.name;
+		$scope.user.fbId = user.id;
+		$scope.user.fbName = user.name;
 
 		ParseService.get('Users', {"facebookId":user.id}, function(results) {
-			$scope.userId = results[0].objectId;
-			$scope.userDescription = results[0].userDescription;
+			$scope.user.id = results[0].objectId;
+			$scope.user.description = results[0].userDescription;
 			navigator.geolocation.getCurrentPosition(function (location) {
-				$scope.userLocation = new Parse.GeoPoint(location.coords.latitude, location.coords.longitude);
+				$scope.user.location = new Parse.GeoPoint(location.coords.latitude, location.coords.longitude);
 				var latlon = location.coords.latitude + "," + location.coords.longitude;
 
-				var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false";
+				var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=350x300&sensor=false";
 
 				document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
 			})
-			$scope.userAge = results[0].userAge;
-			$scope.nickName = results[0].nickName;
+			$scope.user.age = results[0].userAge;
+			$scope.user.nickName = results[0].nickName;
 		});
 	});
 
 	$scope.saveProfile = function() {
-		ParseService.update('Users', $scope.userId,{"userDescription":$scope.userDescription});
-		ParseService.update('Users', $scope.userId,{"userLocation":$scope.userLocation});
-		ParseService.update('Users', $scope.userId,{"userAge":$scope.userAge});
-		ParseService.update('Users', $scope.userId,{"nickName":$scope.nickName});
+		ParseService.update('Users', $scope.user.id,{"userDescription":$scope.user.description});
+		ParseService.update('Users', $scope.user.id,{"userLocation":$scope.user.location});
+		ParseService.update('Users', $scope.user.id,{"userAge":$scope.user.age});
+		ParseService.update('Users', $scope.user.id,{"nickName":$scope.user.nickName});
+		console.log($scope.user.id);
+		console.log($scope.user.description);
+		console.log($scope.user.location);
+		console.log($scope.user.age);
+		console.log($scope.user.nickName);
 	}
 
 	$scope.logout = function() {
